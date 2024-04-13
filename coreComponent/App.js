7949e5f6a125207a46922f1516b37d28a6f70767
sumansauravmay <sumansauravmay12345@gmail.com>
2024-04-13 13:01:15 +0530

    corecomponet

--- coreComponent/App.js
+++ coreComponent/App.js
@@ -1,13 +1,13 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, ImageBackground } from "react-native";
 const pic=require("./assets/icon.png");
 
 export default function App() {
   return <View style={{flex:1, backgroundColor:"red"}}>
 
-    <View style={{width:200, height:200, backgroundColor:"blue"}}>
+    <View style={{width:200, height:100, backgroundColor:"blue"}}>
     </View>
 
-    <View style={{width:200, height:200, backgroundColor:"green", padding: 30}}>
+    <View style={{width:200, height:100, backgroundColor:"green", padding: 30}}>
       <Text>
         <Text style={{color:"white"}}>Hello </Text>
         React Native</Text>
@@ -18,9 +18,14 @@ export default function App() {
 </View>
 
 <View>
-<Image source={{uri:"https://picsum.photos/300"}} style={{height:300, width:300}}/>
+<Image source={{uri:"https://picsum.photos/300"}} style={{height:200, width:300}}/>
 </View>
 
+<View>
+<ImageBackground source={pic} >
+<Text>height</Text>
+</ImageBackground>
+</View>
 
   </View>;
 }

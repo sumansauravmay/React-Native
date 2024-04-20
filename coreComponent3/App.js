cf7a0e32ec72d2fca4e393dda713d2032fadc359
sumansauravmay <sumansauravmay12345@gmail.com>
2024-04-20 15:47:50 +0530

    modal

--- coreComponent3/App.js
+++ coreComponent3/App.js
@@ -1,23 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { Button, Modal, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, Modal, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const [isvisible,setIsvisible]=useState(false);
+  const [isvisible, setIsvisible] = useState(false);
 
   return (
-    <View style={{flex:1, backgroundColor:"plum", padding: 60}}>
-     <Button onPress={()=>setIsvisible(true)} title="Press"/>
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <Button onPress={() => setIsvisible(true)} title="Press" />
 
-
-
-<Modal visible={isvisible} onRequestClose={()=>setIsvisible(true)} animationType='slide' presentationStyle='pageSheet'>
-  <View style={{flex:1, backgroundColor:"lightblue", padding: 60}}>
-<Text>Modal Content</Text>
-<Button onPress={()=>setIsvisible(false)} title='close' color="midnightblue"/>
-  </View>
-</Modal>
-
+      <Modal
+        visible={isvisible}
+        onRequestClose={() => setIsvisible(true)}
+        animationType="slide"
+        presentationStyle="pageSheet"
+      >
+        <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
+          <Text>Modal Content</Text>
+          <Button
+            onPress={() => setIsvisible(false)}
+            title="close"
+            color="midnightblue"
+          />
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -25,9 +31,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
-

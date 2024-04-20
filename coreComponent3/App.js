a72b997a6562023dcde2bf92b5c83adb92f391ac
sumansauravmay <sumansauravmay12345@gmail.com>
2024-04-20 15:39:38 +0530

    modal

--- coreComponent3/App.js
+++ coreComponent3/App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, Modal, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [isvisible,setIsvisible]=useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1, backgroundColor:"plum", padding: 60}}>
+     <Button onPress={()=>setIsvisible(true)} title="Press"/>
+
+
+
+<Modal visible={isvisible}>
+  <View style={{flex:1, backgroundColor:"lightblue", padding: 60}}>
+<Text>Modal Content</Text>
+<Button onPress={()=>setIsvisible(false)} title='close' color="midnightblue"/>
+  </View>
+</Modal>
+
     </View>
   );
 }

5e16901bffbf5ccd21bf332adcdc723d9512d21a
sumansauravmay <sumansauravmay12345@gmail.com>
2024-04-24 18:38:00 +0530

    flex basic

---
+++ RNLayout/App.js
@@ -0,0 +1,30 @@
+
+import { StyleSheet, View } from 'react-native';
+import Box from './component/Box';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    <Box style={{backgroundColor:"#8e9b00"}}>Box 1</Box>
+    <Box style={{backgroundColor:"#b65d1f"}}>Box 2</Box>
+    <Box style={{backgroundColor:"#1c4c56"}}>Box 3</Box>
+    <Box style={{backgroundColor:"#ab9156"}}>Box 4</Box>
+    <Box style={{backgroundColor:"#6b0803"}}>Box 5</Box>
+    <Box style={{backgroundColor:"#1c4c56"}}>Box 6</Box>
+    <Box style={{backgroundColor:"#b95f21"}}>Box 7</Box>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop:60,
+    borderWidth:6,
+    borderColor:"red"
+  },
+});
+
+
+
+
+

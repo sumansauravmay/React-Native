9208efa4407633739bd4c7ca9acfdab0b56151c1
sumansauravmay <sumansauravmay12345@gmail.com>
2024-04-24 19:09:59 +0530

    flex basic

--- RNLayout/App.js
+++ RNLayout/App.js
@@ -4,9 +4,10 @@ import Box from './component/Box';
 
 export default function App() {
   return (
+    // <View style={{backgroundColor:"plum", flex:1}}></View>
     <View style={styles.container}>
-    <Box style={{backgroundColor:"#8e9b00"}}>Box 1</Box>
-    <Box style={{backgroundColor:"#b65d1f"}}>Box 2</Box>
+    <Box style={{backgroundColor:"#8e9b00",flex:1}}>Box 1</Box>
+    <Box style={{backgroundColor:"#b65d1f",flex:3}}>Box 2</Box>
     <Box style={{backgroundColor:"#1c4c56"}}>Box 3</Box>
     <Box style={{backgroundColor:"#ab9156"}}>Box 4</Box>
     <Box style={{backgroundColor:"#6b0803"}}>Box 5</Box>
@@ -18,6 +19,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    flex:1,
     marginTop:60,
     borderWidth:6,
     borderColor:"red"

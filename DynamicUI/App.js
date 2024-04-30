d14f56cad653e2d581217883d7c0a7c5dd61428f
sumansauravmay <sumansauravmay12345@gmail.com>
2024-04-30 22:19:17 +0530

    dymension api

--- DynamicUI/App.js
+++ DynamicUI/App.js
@@ -1,17 +1,43 @@
+import { useEffect, useState } from "react";
 import { StyleSheet, View, Text, Dimensions } from "react-native";
 
 export default function App() {
+  const [dimensions, setDimensions] = useState({
+    window: Dimensions.get("window"),
+  });
+
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener("change", ({ window }) => {
+      setDimensions({ window });
+    });
+    return () => subscription?.remove();
+  });
+
+  const { window } = dimensions;
+  const windowHeight = window.height;
+  const windowWidth = window.width;
+
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text}>Welcome</Text>
+      <View
+        style={[
+          styles.box,
+          {
+            width: windowWidth > 500 ? "70%" : "90%",
+            height: windowHeight > 500 ? "60%" : "90%",
+          },
+        ]}
+      >
+        <Text style={{ fontSize: windowWidth > 500 ? 50 : 24 }}>Welcome</Text>
       </View>
     </View>
   );
 }
 
-const windowWidth = Dimensions.get("window").width;
-const windowHeight = Dimensions.get("window").width;
+// const windowWidth = Dimensions.get("window").width;
+// const windowHeight = Dimensions.get("window").width;
+
+// console.log({ windowHeight, windowWidth });
 
 const styles = StyleSheet.create({
   container: {
@@ -21,13 +47,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   box: {
-    width: windowWidth > 500 ? "70%" : "90%",
-    height: windowHeight > 500 ? "60%" : "90%",
+    // width: windowWidth > 500 ? "70%" : "90%",
+    // height: windowHeight > 500 ? "60%" : "90%",
     backgroundColor: "lightblue",
     alignItems: "center",
     justifyContent: "center",
   },
-  text: {
-    fontSize: windowWidth > 500 ? 50 : 24
-  },
+  // text: {
+  //   fontSize: windowWidth > 500 ? 50 : 24,
+  // },
 });

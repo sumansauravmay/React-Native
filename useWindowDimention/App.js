79e5acb2d71c56f2763fc534ac7895b340f60c27
sumansauravmay <sumansauravmay12345@gmail.com>
2024-05-06 14:08:34 +0530

    useWindowDimension hook

--- useWindowDimention/App.js
+++ useWindowDimention/App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { StyleSheet, View, Text, useWindowDimensions} from "react-native";
+
 
 export default function App() {
+ const windowWidth=useWindowDimensions().width;
+ const windowHeight=useWindowDimensions().height;
+ 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View
+        style={[
+          styles.box,
+          {
+            width: windowWidth > 500 ? "70%" : "90%",
+            height: windowHeight > 500 ? "60%" : "90%",
+          },
+        ]}
+      >
+        <Text style={{ fontSize: windowWidth > 500 ? 50 : 24 }}>Welcome</Text>
+      </View>
     </View>
   );
 }
 
+// const windowWidth = Dimensions.get("window").width;
+// const windowHeight = Dimensions.get("window").width;
+
+// console.log({ windowHeight, windowWidth });
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "plum",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  box: {
+    // width: windowWidth > 500 ? "70%" : "90%",
+    // height: windowHeight > 500 ? "60%" : "90%",
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  // text: {
+  //   fontSize: windowWidth > 500 ? 50 : 24,
+  // },
 });
+
+
+

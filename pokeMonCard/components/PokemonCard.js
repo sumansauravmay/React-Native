7856d0dfe2521887c33343c137f1221b9de52e04
sumansauravmay <sumansauravmay12345@gmail.com>
2024-05-07 16:52:35 +0530

    pokemon card exercise

---
+++ pokeMonCard/components/PokemonCard.js
@@ -0,0 +1,127 @@
+import { View, Text, StyleSheet, Platform, Image } from "react-native";
+
+const getTypeDetails = (type) => {
+  switch (type.toLowerCase()) {
+    case "electric":
+      return { borderColor: "#FFD700", emoji: "⚡️" };
+    case "water":
+      return { borderColor: "#6493EA", emoji: "💧" };
+    case "fire":
+      return { borderColor: "#FF5733", emoji: "🔥" };
+    case "grass":
+      return { borderColor: "#66CC66", emoji: "🌿" };
+    default:
+      return { borderColor: "#A0A0A0", emoji: "❓" };
+  }
+};
+
+export default function PokemonCard(props) {
+  const { name, image, type, hp, moves, weaknesses } = props;
+  const { borderColor, emoji } = getTypeDetails(type);
+
+  return (
+    <View style={styles.card}>
+      <View style={styles.namecontainer}>
+        <Text style={styles.name}>{name}</Text>
+        <Text style={styles.hp}>❤️ {hp}</Text>
+      </View>
+      <Image
+        style={styles.image}
+        source={image}
+        accessibilityLabel={`${name} pokemon`}
+        resizeMode="contain"
+      />
+      <View style={styles.typeContainer}>
+        <View style={[styles.badge, { borderColor }]}>
+          <Text style={styles.typeEmoji}>{emoji}</Text>
+          <Text style={styles.typeText}>{type}</Text>
+        </View>
+      </View>
+
+      <View style={styles.movesContainer}>
+        <Text style={styles.movesText}>Moves:{moves.join(", ")}</Text>
+      </View>
+
+      <View style={styles.weaknescontainer}>
+        <Text style={styles.weaknesText}>weaknesses:{weaknesses.join(", ")}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+    borderRadius: 16,
+    borderWidth: 2,
+    padding: 16,
+    margin: 16,
+    ...Platform.select({
+      ios: {
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: "#333",
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+  namecontainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 32,
+  },
+  name: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  hp: {
+    fontSize: 22,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    marginBottom: 16,
+  },
+  typeContainer: {
+    marginBottom: 40,
+    width: "50%",
+    marginLeft:60
+  },
+  badge: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 20,
+    borderWidth: 4,
+  },
+  typeEmoji: {
+    fontSize: 30,
+    marginRight: 12,
+  },
+  typeText:{
+    fontSize:22,
+    fontWeight:"bold"
+  },
+  movesContainer:{
+    marginBottom:16
+  },
+  movesText:{
+    fontSize:22,
+    fontWeight:"bold"
+  },
+  weaknescontaine:{
+    marginBottom:16
+  },
+  weaknesText:{
+    fontSize:22,
+    fontWeight:"bold"
+  }
+});
+
+
+
+

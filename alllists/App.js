cfed035ca9f5cf9d9dafc6386b43281541ab69fd
sumansauravmay <sumansauravmay12345@gmail.com>
2024-05-07 23:44:41 +0530

    lists

--- alllists/App.js
+++ alllists/App.js
@@ -1,19 +1,28 @@
-
-import { StyleSheet, Text, View, ScrollView, SafeAreaView, StatusBar } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  SafeAreaView,
+  StatusBar,
+  FlatList
+} from "react-native";
 import pokemonList from "./data.json";
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.scrollView}>
-      {
-        pokemonList.map((pokemon)=>( 
-          <View key={pokemon.id} style={styles.card}>
-            <Text style={styles.cardText}>{pokemon.name}</Text>
-            <Text>{pokemon.type}</Text>
+      <View style={styles.scrollView}>
+      <FlatList 
+      data={pokemonList}
+      renderItem={({item})=>(
+        <View key={item.id} style={styles.card}>
+            <Text style={styles.cardText}>{item.name}</Text>
+            <Text>{item.type}</Text>
           </View>
-        ))
-      }
-      </ScrollView>
+      )}
+      keyExtractor={(item, index)=>item.id.toString()}
+      />
+      </View>
     </SafeAreaView>
   );
 }
@@ -21,20 +30,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f5f5f5',
-    paddingTop:StatusBar.currentHeight
+    backgroundColor: "#f5f5f5",
+    paddingTop: StatusBar.currentHeight,
   },
-  scrollView:{
-paddingHorizontal:16
+  scrollView: {
+    paddingHorizontal: 16,
   },
-  card:{
-    backgroundColor:"white",
-    padding:16,
-    borderRadius:8,
-    borderWidth:1,
-    marginBottom:16
+  card: {
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    marginBottom: 16,
   },
-  cardText:{
-    fontSize:30
-  }
+  cardText: {
+    fontSize: 30,
+  },
 });

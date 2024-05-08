1df4a8a1e12faca252f511980653249beca9fd92
sumansauravmay <sumansauravmay12345@gmail.com>
2024-05-08 17:08:38 +0530

    input text

--- inputForm/App.js
+++ inputForm/App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  SafeAreaView,
+  TextInput,
+  Switch,
+} from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [isDark, setIsDark] = useState(false);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+        placeholder="Enter Email"
+        // secureTextEntry
+        // keyboardType="numeric"
+        autoCorrect={false}
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={[styles.input, styles.multilineText]}
+        placeholder="message"
+        multiline
+      />
+      <Text style={styles.text}>My name is {name}</Text>
+
+      <View style={styles.switchContainer}>
+        <Text style={styles.text}>Dark Mode</Text>
+        <Switch
+          value={isDark}
+          onValueChange={() => setIsDark((previos) => !previos)}
+          trackColor={{ false: "#767577", true: "lightblue" }}
+          thumbColor="#f4f3f4"
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: StatusBar.currentHeight,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    padding: 10,
+    borderWidth: 1,
+  },
+  text: {
+    fontSize: 30,
+    padding: 10,
+  },
+  multilineText: {
+    minHeight: 100,
+    textAlignVertical: "top",
+  },
+  switchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
   },
 });
